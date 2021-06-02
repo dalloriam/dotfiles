e9f09ebcedc0f5cdf63942ae1718a9bff716b821
William Dussault <dalloriam@gmail.com>
2021-06-01 20:19:04 -0400

    Remove sudo from rust script

--- scripts/rust.fish
+++ scripts/rust.fish
@@ -1,3 +1,3 @@
 if not type -q cargo
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo sh -s -- -y
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 end
