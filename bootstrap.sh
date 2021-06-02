374fbf35172b7062f8112207187b51c51c066bbc
William Dussault <dalloriam@gmail.com>
2021-06-01 22:46:51 -0400

    Add boostrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+version=`curl -XGET https://api.github.com/repos/dalloriam/dotfiles/releases/latest | grep -o 'v.\..\..' | head -n 1`
+os=`uname`
+url="https://github.com/dalloriam/dotfiles/releases/download/$version/bootstrap-$os-amd64"
+
+curl -L $url > ./.bootstrap
+chmod +x ./.bootstrap
+./.bootstrap
+rm ./.bootstrap
