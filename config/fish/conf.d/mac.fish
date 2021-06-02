0e2aeec6b0099ab2d17f828b1910660b28744f38
William Dussault <dalloriam@gmail.com>
2021-06-01 22:56:03 -0400

    Add setting to disable smart quotes on mac

--- config/fish/conf.d/mac.fish
+++ config/fish/conf.d/mac.fish
@@ -1,7 +1,10 @@
 if test (uname) = "Darwin"
-	set PATH $PATH /usr/local/bin
-	set PATH $PATH /usr/local/go/bin
+    set PATH $PATH /usr/local/bin
+    set PATH $PATH /usr/local/go/bin
 
-	set PATH ~/Library/Python/3.8/bin $PATH
-	set PATH /opt/homebrew/bin $PATH
+    set PATH ~/Library/Python/3.8/bin $PATH
+    set PATH /opt/homebrew/bin $PATH
+
+    # Disable smart quotes.
+    defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 end
