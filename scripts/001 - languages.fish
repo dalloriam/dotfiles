1debe9eb9f554b497825a1a45378b9aebb00b283
William Dussault <william@dussault.dev>
2022-02-23 09:50:37 -0500

    Import kitty themes & fix go install script

--- scripts/001 - languages.fish
+++ scripts/001 - languages.fish
@@ -7,12 +7,14 @@ end
 if test (uname) = Linux
     set goversion "1.17.7"
 
-    if test (uname -m) = aarch64
+    set osarch (uname -m)
+    if test $osarch = aarch64
         set arch arm64
-    else
-        set arch (uname -m)
+    else if test $osarch = x86_64
+        set arch amd64
     end
 
     wget https://go.dev/dl/go$goversion.linux-$arch.tar.gz
     sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go$goversion.linux-$arch.tar.gz
+    rm go$goversion.linux-$arch.tar.gz
 end
