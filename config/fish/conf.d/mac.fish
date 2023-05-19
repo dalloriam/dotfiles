cf339e5aee6a4efa8f582faa092ee306954beae3
William Dussault <dalloriam@gmail.com>
2023-05-19 17:46:55 -0400

    mac change

--- config/fish/conf.d/mac.fish
+++ config/fish/conf.d/mac.fish
@@ -1,9 +1,10 @@
-if test (uname) = "Darwin"
+if test (uname) = Darwin
     set PATH $PATH /usr/local/bin
     set PATH $PATH /usr/local/go/bin
 
     set PATH ~/Library/Python/3.9/bin $PATH
     set PATH /opt/homebrew/bin $PATH
+    set PATH (brew --prefix)/opt/llvm/bin $PATH
 
     # The next line updates PATH for the Google Cloud SDK.
     if [ -f '/Users/wduss/.gcloud/path.fish.inc' ]
