938af609aff5f41e304360e5a647e18cadd07714
William Dussault <william@dussault.dev>
2025-09-06 15:34:52 -0400

    Minor tweaks

--- data/dark-mode.d/desktop-notification.sh
+++ data/dark-mode.d/desktop-notification.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env fish
+swww img $HOME/pictures/wallpaper/dark.png
+sleep 2
 gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
 
 niri msg action do-screen-transition && gsettings set org.gnome.desktop.interface color-scheme prefer-dark
@@ -6,7 +8,5 @@ niri msg action do-screen-transition && gsettings set org.gnome.desktop.interfac
 makoctl mode -s dark
 
 ln -sf "$HOME"/.config/fuzzel/dark.ini "$HOME"/.config/fuzzel/fuzzel.ini
-
-swww img $HOME/pictures/wallpaper/dark.png
 
 notify-send --app-name="darkman" --urgency=low --icon=weather-clear "switching to dark mode"
--- data/light-mode.d/desktop-notification.sh
+++ data/light-mode.d/desktop-notification.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env fish
+swww img $HOME/pictures/wallpaper/light.png
+sleep 2
 
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita
 niri msg action do-screen-transition && gsettings set org.gnome.desktop.interface color-scheme prefer-light
@@ -6,7 +8,5 @@ niri msg action do-screen-transition && gsettings set org.gnome.desktop.interfac
 makoctl mode -r dark
 
 ln -sf "$HOME"/.config/fuzzel/light.ini "$HOME"/.config/fuzzel/fuzzel.ini
-
-swww img $HOME/pictures/wallpaper/light.png
 
 notify-send --app-name="darkman" --urgency=low --icon=weather-clear "switching to light mode"
